8ce288c6e87dd7e3f833ab89b908a76eca69fdf1
uglymango <User@DESKTOP-LOEF761>
2023-07-27 19:52:16 +0400

    Added the class "SalesItem"

---
+++ MarketSystems/Services/Concrete/SalesItem.cs
@@ -0,0 +1,15 @@
+﻿namespace MarketConsole.Data.Models
+{
+    public class SalesItem
+    {
+        public SalesItem(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
+
