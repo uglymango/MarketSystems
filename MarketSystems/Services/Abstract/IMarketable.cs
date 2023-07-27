653410f915b9a8ddec88c1dfedbcb1e5313ac1b6
uglymango <User@DESKTOP-LOEF761>
2023-07-27 19:51:27 +0400

    Added the methods to iMarketable

---
+++ MarketSystems/Services/Abstract/IMarketable.cs
@@ -0,0 +1,21 @@
+﻿using MarketConsole.Data.Models;
+using MarketSystems.Data.Enums;
+using MarketSystems.Data.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MarketConsole.Services.Abstract
+{
+    public interface IMarkettable
+    {
+        List<Product> GetProducts();
+        List<Sale> GetSale();
+        int AddProduct(string name, decimal price, ProductCategory category, int counts);
+        void DeleteProduct(int ID);
+        void UpdateProduct(int ID, string name, decimal price, ProductCategory category, int counts);
+        List<Product> ShowCategoryByProduct(ProductCategory category);
+        List<Product> FindProductByName(string name);
+        List<Product> ShowProductByPriceRange(decimal minPrice, decimal maxPrice);
+        void AddNewSale(int id, int count, DateTime dateTime);
+    }
+}
