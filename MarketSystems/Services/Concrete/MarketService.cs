9a43507db547ff909554ae1edd0fec49f9197865
uglymango <User@DESKTOP-LOEF761>
2023-07-30 22:19:29 +0400

    All methods in "ManageSales" and "ManageProducts" classes are now in "MarketService" and everything works perfectly.

--- MarketSystems/Services/Concrete/MarketService.cs
+++ MarketSystems/Services/Concrete/MarketService.cs
@@ -33,18 +33,133 @@ namespace MarketConsole.Services.Concrete
             saleItems = new List<SaleItem>();
         }
 
-        public int AddProduct(string name, decimal price, ProductCategory category, int quantity)
+        public void RemoveProductFromSale()
         {
+            try
+            {
+                Console.WriteLine("Enter the sale ID to remove the product:");
+                int saleId = int.Parse(Console.ReadLine());
+
+                var sale = sales.Find(s => s.Id == saleId);
+
+                if (sale != null)
+                {
+                    foreach (var saleItem in sale.SaleItems)
+                    {
+                        var product = saleItem.Product;
+                        product.Quantity += saleItem.Quantity;
+                    }
+
+                    sales.Remove(sale);
+                    Console.WriteLine("Product removed from the sale successfully!");
+                    Console.WriteLine("------------------------");
+                }
+                else
+                {
+                    Console.WriteLine("Sale not found with the specified ID.");
+                    Console.WriteLine("------------------------");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Oops, an error occurred: {ex.Message}");
+                Console.WriteLine("------------------------");
+            }
+        }
+
+        public void ReturnPurchase()
+        {
+            try
+            {
+                Console.WriteLine("Enter the sale ID to return the purchase:");
+                int saleId = int.Parse(Console.ReadLine());
+
+                var sale = sales.FirstOrDefault(s => s.Id == saleId);
+
+                if (sale != null)
+                {
+                    if (sale.SaleItems != null)
+                    {
+                        foreach (var saleItem in sale.SaleItems)
+                        {
+                            var product = saleItem.Product;
+                            product.Quantity += saleItem.Quantity;
+                        }
+                    }
+
+                    sales.Remove(sale);
+                    Console.WriteLine("Purchase returned successfully!");
+                    Console.WriteLine("------------------------");
+                }
+                else
+                {
+                    Console.WriteLine("Sale not found with the specified ID.");
+                    Console.WriteLine("------------------------");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while returning purchase: {ex.Message}");
+                Console.WriteLine("------------------------");
+            }
+        }
+
+        public List<Sale> ShowSalesByDate(DateTime minDate, DateTime maxDate)
+        {
+            if (minDate > maxDate)
+                throw new ArgumentException("Minimum date cannot be greater than Maximum date!");
+
+            List<Sale> foundSales = sales.FindAll(sale => sale.Date >= minDate && sale.Date <= maxDate);
+
+            return foundSales;
+        }
+
+        public List<Sale> ShowSalesOnExactDate(DateTime dateTime)
+        {
+            List<Sale> salesOnExactDate = sales.FindAll(sale => sale.Date.Date == dateTime.Date);
+
+            return salesOnExactDate;
+        }
+
+        public List<Sale> ShowSalesByPriceRange(decimal minAmount, decimal maxAmount)
+        {
+            List<Sale> foundSales = sales.FindAll(sale => sale.Amount >= minAmount && sale.Amount <= maxAmount);
+
+            return foundSales;
+        }
+
+        public List<ProductCategory> GetProductCategories()
+{
+    return Enum.GetValues(typeof(ProductCategory)).Cast<ProductCategory>().ToList();
+}
+
+        public int AddProductWithCategory(string name, decimal price, string category, int quantity)
+        {
+            if (!Enum.TryParse<ProductCategory>(category, true, out var productCategory))
+            {
+                Console.WriteLine("Invalid product category.");
+                return -1;
+            }
+
             if (string.IsNullOrWhiteSpace(name))
-                throw new ArgumentException("Product name cannot be null or whitespace!");
+            {
+                Console.WriteLine("Product name cannot be null or whitespace!");
+                return -1;
+            }
 
             if (price < 0)
-                throw new ArgumentException("Price cannot be negative!");
+            {
+                Console.WriteLine("Price cannot be negative!");
+                return -1;
+            }
 
             if (quantity < 0)
-                throw new ArgumentException("Product count cannot be less than 0.");
+            {
+                Console.WriteLine("Product count cannot be less than 0.");
+                return -1;
+            }
 
-            var product = new Product(name, price, category, quantity);
+            var product = new Product(name, price, productCategory, quantity);
 
             products.Add(product);
             return product.Id;
@@ -83,34 +198,7 @@ namespace MarketConsole.Services.Concrete
             existingProduct.Quantity = quantity;
         }
 
-        public void ShowCategoryByProduct()
-        {
-            try
-            {
-                var productsByCategory = products.GroupBy(p => p.Category);
-
-                foreach (var categoryGroup in productsByCategory)
-                {
-                    var category = categoryGroup.Key;
-                    var productsInCategory = categoryGroup.ToList();
-
-                    Console.WriteLine($"Category: {category}");
-
-                    foreach (var product in productsInCategory)
-                    {
-                        Console.WriteLine($" Name: {product.Name}, Price: {product.Price}, Quantity: {product.Quantity}");
-                    }
-
-                    Console.WriteLine("------------------------");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Oops, an error occurred: {ex.Message}");
-                Console.WriteLine("------------------------");
-            }
-        }
-    public List<Product> ShowCategoryByProduct(ProductCategory category)
+        public List<Product> ShowCategoryByProduct(ProductCategory category)
         {
             if (category == null)
                 throw new ArgumentNullException("Category cannot be null!");
@@ -137,48 +225,19 @@ namespace MarketConsole.Services.Concrete
             return products.Where(x => x.Price >= minPrice && x.Price <= maxPrice).ToList();
         }
 
-        public void ShowSales()
+        public void AddNewSale(int id, int count, DateTime dateTime)
         {
-            try
-            {
-                if (sales.Count == 0)
-                {
-                    Console.WriteLine("There are no sales!");
-                    return;
-                }
-
-                Console.WriteLine("All Sales:");
-                var table = new ConsoleTable("ID", "Date", "Product", "Total Price");
-
-                foreach (var sale in sales)
-                {
-                    string productNames = string.Join(", ", sale.SaleItems.Select(saleItem => saleItem.Product.Name));
-                    table.AddRow(sale.Id, sale.Date.ToString("yyyy-MM-dd"), productNames, sale.Amount);
-                }
-
-                table.Write();
-                Console.WriteLine("------------------------");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Oops, an error occurred: {ex.Message}");
-                Console.WriteLine("------------------------");
-            }
-        }
-
-        public void AddNewSale(int id, int quantity, DateTime dateTime)
-        {
-            if (quantity <= 0)
+            if (count <= 0)
                 throw new ArgumentOutOfRangeException("Quantity cannot be negative!");
 
             var product = products.Find(x => x.Id == id);
 
-            if (product != null && product.Quantity >= quantity)
+            if (product != null && product.Quantity >= count)
             {
-                var price = product.Price * quantity;
-                product.Quantity -= quantity;
+                var price = product.Price * count;
+                product.Quantity -= count;
 
-                var saleItem = new SaleItem(product, quantity);
+                var saleItem = new SaleItem(product, count);
 
                 var saleItemsList = new List<SaleItem> { saleItem };
 
@@ -191,74 +250,14 @@ namespace MarketConsole.Services.Concrete
             }
         }
 
-        public void RemoveSale(int saleId)
+        public List<Sale> ShowSalesByID(int Id)
         {
-            try
-            {
-                var sale = sales.Find(s => s.Id == saleId);
-                if (sale != null)
-                {
-                    foreach (var saleItem in sale.SaleItems)
-                    {
-                        var product = saleItem.Product;
-                        product.Quantity += saleItem.Quantity;
-                    }
-
-                    sales.Remove(sale);
-                    Console.WriteLine("Sale removed successfully!");
-                    Console.WriteLine("------------------------");
-                }
-                else
-                {
-                    Console.WriteLine("Sale not found with the specified ID.");
-                    Console.WriteLine("------------------------");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Oops, an error occurred: {ex.Message}");
-                Console.WriteLine("------------------------");
-            }
-        }
-
-        public List<Sale> ShowSalesByDate(DateTime minDate, DateTime maxDate)
-        {
-            if (minDate > maxDate)
-                throw new ArgumentException("Minimum date cannot be greater than Maximum date!");
-
-            List<Sale> foundSales = sales.FindAll(sale => sale.Date >= minDate && sale.Date <= maxDate);
-
-            return foundSales;
-        }
-
-        public List<Sale> ShowSaleByPriceRange(decimal minPrice, decimal maxPrice)
-        {
-            if (minPrice < 0 || maxPrice < 0 || minPrice > maxPrice)
-                throw new ArgumentException("Invalid price range!");
-
-            List<Sale> salePriceRange = sales.FindAll(sale => sale.Amount >= minPrice && sale.Amount <= maxPrice);
-
-            return salePriceRange;
-        }
-
-        public List<Sale> ShowSalesByID(int saleID)
-        {
-            List<Sale> salesWithMatchingID = sales.FindAll(sale => sale.Id == saleID);
+            List<Sale> salesWithMatchingID = sales.FindAll(sale => sale.Id == Id);
 
             return salesWithMatchingID;
         }
-
-        public List<Sale> ShowSalesOnExactDate(DateTime dateTime)
-        {
-            List<Sale> salesOnExactDate = sales.FindAll(sale => sale.Date.Date == dateTime.Date);
-
-            return salesOnExactDate;
-        }
-
-        internal object FindProductById(int productID)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
+
+
 
